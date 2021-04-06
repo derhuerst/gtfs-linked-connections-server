e8d62c512eeb2f01f12cf5742e6ba53b1f118ca7
Jannis R <mail@jannisr.de>
2021-04-06 15:23:28 +0200

    HTTP API skeleton

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 'use strict'
 
-// todo
+const serveLinkedConnections = require('./lib/serve-linked-connections')
+
+const serveGtfsAsLinkedConnections = (opt = {}) => {
+	const findConnections = async (todo) => {
+		// todo
+		return []
+	}
+
+	return serveLinkedConnections({
+		...opt,
+		findConnections,
+	})
+}
+
+module.exports = serveGtfsAsLinkedConnections
