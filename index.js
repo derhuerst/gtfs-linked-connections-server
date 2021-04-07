a26b3d2127e5188703484e78427ab8582b289d45
Jannis R <mail@jannisr.de>
2021-04-07 20:30:41 +0200

    finish basic LC connections API

--- index.js
+++ index.js
@@ -1,20 +1,54 @@
 'use strict'
 
 const {Client: PgClient} = require('pg')
+const {stringify: encodeQuery} = require('querystring')
+const omit = require('lodash/omit')
 const serveLinkedConnections = require('./lib/serve-linked-connections')
+
+// This follows http://vocab.gtfs.org/gtfs.ttl as of 2021-04-06
+// see also https://web.archive.org/web/20201010044144/http://vocab.gtfs.org/gtfs.ttl
+// todo: adapt to the current Linked GTFS spec on GitHub
+// https://github.com/OpenTransport/linked-gtfs/blob/fc12d51feed4a29cb83384b360de5df8c5c5a7db/spec.md
+
+const defaultStopId = (s) => {
+	return s.stop_id
+}
+
+const defaultConnectionId = (c) => {
+	return Math.random().toString(16).slice(2) // todo
+}
+
+const defaultTripId = (t) => {
+	return t.trip_id
+}
+
+const defaultRouteId = (r) => {
+	return r.route_id
+}
 
 const serveGtfsAsLinkedConnections = async (opt = {}) => {
 	const {
 		getDbClient,
+		stopId,
+		connectionId,
+		tripId,
+		routeId,
 	} = {
 		getDbClient: async () => {
 			const db = new PgClient()
 			await db.connect()
 			return db
 		},
-		// todo: hooks to transform stop/station/route/trip/connection IDs
+		// todo: hooks to transform a formatted stop/station/route/trip/connection
+		stopId: defaultStopId,
+		connectionId: defaultConnectionId,
+		tripId: defaultTripId,
+		routeId: defaultRouteId,
 		...opt,
 	}
+
+	// todo: let `serveLinkedConnections` pass this in here
+	const connectionsUrl = query => `/connections?${encodeQuery(query)}`
 
 	const db = await getDbClient()
 
@@ -24,27 +58,32 @@ const serveGtfsAsLinkedConnections = async (opt = {}) => {
 	}
 	const formatStop = (s) => {
 		return {
-			'id': s.stop_id, // todo: rename key?
+			'@id': `/stops/${encodeURIComponent(stopId(s))}`,
+			'@type': 'gtfs:Stop',
 			'rdfs:label': s.stop_name || null,
 			'geosparql:asWkt': formatCoords(s.stop_lon, s.stop_lat),
-			'gtfs:parentStation': s.station_id ? {
-				'id': s.station_id, // todo: rename key?
+			'parentStation': s.station_id ? {
+				'@id': `/stops/${encodeURIComponent(s.station_id)}`, // todo: use stopId()
+				'@type': 'gtfs:Station',
 				'rdfs:label': s.station_name || null,
 				'geosparql:asWkt': formatCoords(s.station_lon, s.station_lat),
-				'gtfs:code': s.station_code || null,
-				'gtfs:wheelchairBoarding': s.station_wheelchair_boarding || null,
+				'code': s.station_code || null,
+				'wheelchairBoarding': s.station_wheelchair_boarding || null,
 			} : null,
-			'gtfs:code': s.stop_code || null,
-			'gtfs:wheelchairBoarding': s.wheelchair_boarding || null,
-			'gtfs:platformCode': s.platform_code || null,
+			'code': s.stop_code || null,
+			'wheelchairBoarding': s.wheelchair_boarding || null,
+			'platformCode': s.platform_code || null,
+			// todo: timeZone
 		}
 	}
 
 	const formatConnection = (c) => {
 		return {
-			'lc:departureTime': c.t_departure,
-			'lc:departureDelay': null,
-			'lc:departureStop': formatStop({
+			'@id': `/connections/${encodeURIComponent(connectionId(c))}`,
+			'@type': 'lc:Connection',
+			'departureTime': c.t_departure,
+			'departureDelay': null,
+			'departureStop': formatStop({
 				stop_id: c.from_stop_id,
 				stop_name: c.from_stop_name,
 				// todo: stop_lon, stop_lat
@@ -54,9 +93,9 @@ const serveGtfsAsLinkedConnections = async (opt = {}) => {
 				// todo: station_lon, station_lat
 				// todo: station_wheelchair_boarding
 			}),
-			'lc:arrivalTime': c.t_arrival,
-			'lc:arrivalDelay': null,
-			'lc:departureStop': formatStop({
+			'arrivalTime': c.t_arrival,
+			'arrivalDelay': null,
+			'arrivalStop': formatStop({
 				stop_id: c.to_stop_id,
 				stop_name: c.to_stop_name,
 				// todo: stop_lon, stop_lat
@@ -67,9 +106,16 @@ const serveGtfsAsLinkedConnections = async (opt = {}) => {
 				// todo: station_wheelchair_boarding
 			}),
 			'gtfs:trip': {
-				'id': c.trip_id, // todo: rename key?
+				// todo: pass full trip into tripId()
+				'@id': `/trips/${encodeURIComponent(tripId({trip_id: c.trip_id}))}`,
+				'@type': 'gtfs:Trip',
 				'gtfs:route': {
-					'id': c.route_id, // todo: rename key?
+					// todo: pass full route into routeId()
+					'@id': `/routes/${encodeURIComponent(routeId({route_id: c.route_id}))}`,
+					// todo: route_short_name as gtfs:shortName
+					// todo: route_long_name as gtfs:longName
+					// todo: route_route_type as gtfs:routeType
+					// todo: route.agency_id as gtfs:agency
 				},
 				// todo: trip.short_name as gtfs:shortName
 				// todo: trip.wheelchair_accessible
@@ -113,11 +159,37 @@ const serveGtfsAsLinkedConnections = async (opt = {}) => {
 			`,
 			values,
 		})
+		const lastRow = res.rows[res.rows.length - 1]
+		// todo: what if there are 0 rows?
 
 		return {
 			connections: res.rows.map(formatConnection),
-			// todo
-			relations: [],
+			// todo: sth like version or Last-Modified
+			relations: [{
+				// todo: more than `LIMIT` connections with one t_dep? add offset?
+				'@type': 'tree:GreaterThanOrEqualToRelation',
+				'tree:node': connectionsUrl({
+					...omit(query, ['lc:arrivalTime']),
+					'lc:departureTime': lastRow.t_departure.toISOString(),
+				}),
+				'tree:path': 'lc:departureTime',
+				'tree:value': {
+					"@type": "http://www.w3.org/2001/XMLSchema#dateTime",
+					"@value": lastRow.t_departure.toISOString(),
+				}
+			}, {
+				// todo: more than `LIMIT` connections with one t_arr? add offset?
+				'@type': 'tree:SmallerThanOrEqualToRelation',
+				'tree:node': connectionsUrl({
+					...omit(query, ['lc:departureTime']),
+					'lc:arrivalTime': lastRow.t_arrival.toISOString(),
+				}),
+				'tree:path': 'lc:arrivalTime',
+				'tree:value': {
+					"@type": "http://www.w3.org/2001/XMLSchema#dateTime",
+					"@value": lastRow.t_arrival.toISOString(),
+				}
+			}],
 		}
 	}
 
