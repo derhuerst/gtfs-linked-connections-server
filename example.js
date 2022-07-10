d7c87be9b8feadb2f09bafc0f45e90f4b28d84a4
Jannis R <mail@jannisr.de>
2022-07-10 16:34:08 +0200

    example code 📝

--- example.js
+++ example.js
@@ -1,3 +1,21 @@
 'use strict'
 
-// todo
+const {createServer} = require('http')
+const {promisify} = require('util')
+const gtfsLinkedConnectionsServer = require('.')
+
+const PORT = parseInt(process.env.PORT || '3000')
+
+;(async () => {
+	const app = await gtfsLinkedConnectionsServer()
+
+	const server = createServer(app)
+	const pListen = promisify(server.listen.bind(server))
+
+	await pListen(PORT)
+	console.info(`Linked Connections server listening on port ${PORT}`)
+})()
+.catch((err) => {
+	console.error(err)
+	process.exit(1)
+})
